251e7385a88136d1cf09a3265495f4375f3273a5
hemanthlm10-ux <hemanthlm30@gmail.com>
2026-03-05 03:02:13 +0530

    Add files via upload

---
+++ datatypes and operation/Dataoperation.java
@@ -0,0 +1,25 @@
+public class Dataoperation {
+public static void main(String[] args) {
+int a = 20;
+int b = 10;
+int c = 30;
+boolean d= true;
+boolean e = false;
+
+
+        
+System.out.println("addition " + (a + b));
+System.out.println("subtraction " + (a - b));
+System.out.println("multiplication  " + (a * b));
+System.out.println("division  " + (a / b));
+System.out.println("modulus  " + (a % b));
+System.out.println("original value " + c);
+System.out.println("increment  " + (++c));
+System.out.println("decrement  " + (--c));
+System.out.println("and  " + (e && d));
+System.out.println(" or   " + (e || d));
+System.out.println("not d   " + (!d));
+        
+    }    
+
+}
---
+++ datatypes and operation/Datatypes.java
@@ -0,0 +1,21 @@
+public class Datatypes {
+public static void main(String[] args){
+
+int a=6;
+String b="hello";
+boolean c= true;
+double d=1.22; 
+float e=1.28f;
+char f= 'a';
+
+    System.out.println(" String datatype: " + b);
+    System.out.println("  int datatype: " + a);
+    System.out.println(" float datatype: " + e);
+    System.out.println(" double datatype: " + d);
+    System.out.println("char datatype: " + f);
+}
+
+
+
+
+}
